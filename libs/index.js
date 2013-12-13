409dc7bd8c5f4a2ff54210008a09bb34c9e5e76c
Nanako Schattener <nanako@nanako-net.info>
2013-12-13 15:01:24 +0900

    ダイナミックヘルパとメニューの状態遷移に対応

--- libs/index.js
+++ libs/index.js
@@ -5,10 +5,9 @@
 exports.login_status = function(req, res) {
   console.log(req.session.username);
   if (req.session.username) {
-    return '' + req.session.username + ' '
-      + '<a href="/sessions/destroy" class="ui-btn-right">logout</a>';
+    return true;
   }
   else {
-    return '<a href="/sessions/new" class="ui-btn-right">login</a>';
+    return false;
   };
 };
